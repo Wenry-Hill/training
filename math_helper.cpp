bce8c1e157d0bc9ac9fb1c893b19c01e6a047a14
Krypton Cougars <kryptoncougars2539@gmail.com>
2014-06-26 19:03:14 -0400

    The begining of math_helper was started -Henry

--- math_helper.cpp
+++ math_helper.cpp
@@ -11,10 +11,41 @@
  * program will print out how many correct and incorrect answers there were, and
  * then exit.
  */
-
+#include "iostream"
 #include "lib/helpers.h"
 
 int main()
 {
+  seedRandomNumber();
+  int a = getRandomNumber(2,100);
+  std::cout << a << " THIS IS YOUR NUMBER\n";
+  std::cout << "Determine if the number is prime or composite\n";
+  std::cout << "If Prime type P\n";
+  std::cout << "If Composite type C\n";
+  
+  std::string getLetter(bool ignoreCase=true, std::string min="C", std::string max="P");
+  std::string Letter = getLetter("C", "P");
+
+  
+  if (Letter == "P")
+  {
+    std::vector<unsigned int> Answer = getPrimeFactorization(a);
+    std::vector<unsigned int> getPrimeFactorization(unsigned int number);
+
+    if (Answer.size() == 1)
+    {
+      std::cout << "You are correct =)\n";
+    }
+
+  }
+  else
+  {
+    std::cout << "Why you so stupad?\n";
+  }
+
+  
+  
+  
+  
     return 0;
 }
